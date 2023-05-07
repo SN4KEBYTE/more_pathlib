8e7b69ae928729fa3dd0bf3897425744d21afe6f
Timur Kasimov <timurcat7@gmail.com>
2023-05-07 21:43:42 +0700

    ignore_errors param was removed. Fixed subdirs removal

--- more_pathlib/rm.py
+++ more_pathlib/rm.py
@@ -1,25 +1,23 @@
 from pathlib import Path
 
+from more_pathlib.validation import validate_directory
+
 
 def rmtree(  # noqa: WPS231
     path: Path,
     *,
     keep_root: bool = False,
-    ignore_errors: bool = True,
 ) -> None:
+    validate_directory(path)
+
     for child in path.iterdir():
         if child.is_file():
-            child.unlink(missing_ok=ignore_errors)
+            child.unlink()
         else:
             rmtree(
                 child,
-                keep_root=keep_root,
-                ignore_errors=ignore_errors,
+                keep_root=False,
             )
 
     if not keep_root:
-        try:
-            path.rmdir()
-        except FileNotFoundError as err:
-            if not ignore_errors:
-                raise err
+        path.rmdir()
